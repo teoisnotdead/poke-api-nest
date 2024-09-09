f379c4f8eb5d835ff56089548f353146b0b0fd46
TEO <alfredo.ssm@gmail.com>
2024-09-08 23:31:07 -0300

    Se crea resource para poblar la bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
