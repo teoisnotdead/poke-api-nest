fe692fe646f313baf77dea3bf9adf505320c5e07
TEO <alfredo.ssm@gmail.com>
2024-09-15 23:52:39 -0300

    Se crea un custom provider usando patron adaptador, en caso de cambiar axios por fetch o cualquier otro

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { PokemonService } from 'src/pokemon/pokemon.service';
-import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
 
   constructor(
     @InjectModel(Pokemon.name)
     private readonly PokemonModel: Model<Pokemon>,
+    private readonly axiosAdapter: AxiosAdapter,
   ) {}
 
   async executeSeed() {
 
     await this.PokemonModel.deleteMany({}); // Delete all documents
 
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650',
-    );
+    const data = await this.axiosAdapter.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     const pokemonToInsert: {name: string, no: number}[] = [];
 
