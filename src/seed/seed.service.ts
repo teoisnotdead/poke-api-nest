89ca92ac6918d61e9372406c520105217be8b081
TEO <alfredo.ssm@gmail.com>
2024-09-15 23:10:11 -0300

    Se integra axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return `Seed executed`;
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=5');
+
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const id:number = +segments[segments.length - 2];
+
+      console.log({id, name});
+
+    });
+
+    return data.results;
   }
 
 }
